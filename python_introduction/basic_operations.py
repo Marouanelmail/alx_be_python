cc2b086f33777f4718a6603cf58efa0ce8f5e0e0
marouanelmail <elmail0.1marouan@gmail.com>
2024-07-13 13:58:19 +0100

    more calculations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,8 @@
 number1 = 10
 number2 = 5
-result = number1 - number2
-print(result)
-
-
+result1 = number1 - number2
+result2 = number1 + number2
+result3 = number1 * number2
+print(result1)
+print(result2)
+print(result3)
