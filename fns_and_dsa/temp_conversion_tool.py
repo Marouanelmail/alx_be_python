b0b2afb29528c921d23fd47d6a2c297ef627860a
marouanelmail <elmail0.1marouan@gmail.com>
2024-07-17 16:49:40 +0100

    temp converture

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,20 +5,19 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 temp = int(input("Enter the temperature to convert: "))
 
-# def convert_to_celsius(fahrenheit):
-#     global CELSIUS_TO_FAHRENHEIT_FACTOR
-#     celisius = (fahrenheit * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-#     return celisius
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    # global CELSIUS_TO_FAHRENHEIT_FACTOR
+    celisius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celisius
+# def convert_to_celsius(fahrenheit):
+#     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
-# def convert_to_fahrenheit(celisius):
-#     global FAHRENHEIT_TO_CELSIUS_FACTOR
-#     fahrenheit = celisius - 32
-#     result = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
-#     return result
-def convert_to_fahrenheit(celsius):
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+def convert_to_fahrenheit(celisius):
+    # global FAHRENHEIT_TO_CELSIUS_FACTOR
+    fahrenheit = (celisius* CELSIUS_TO_FAHRENHEIT_FACTOR) +32
+    return fahrenheit
+# def convert_to_fahrenheit(celsius):
+#     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 cel_or_fahr = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 match cel_or_fahr:
