997bf5ddd29aa124eee11100294f2d793d722afc
marouanelmail <elmail0.1marouan@gmail.com>
2024-07-20 17:58:26 +0100

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class BankAccount:
+    def __init__(self, account_balance) -> None:
+        self.account_balance = account_balance
+    
+    def deposit(self, amount):
+        # total_balance = self.account_balance + amount
+        # return total_balance
+        self.account_balance += amount
+    
+    def withdraw(self, amount):
+        if self.account_balance < amount:
+            print ("Funds not sufficient")
+            return False
+        elif self.account_balance > amount:
+            self.account_balance -= amount
+            return True   
+    def display_balance(self):
+        print(f"This is your current balance : {self.account_balance}")
+        # self.name = name
+    # def printuser(self):
+    #     print(f"this is {self.name} account balance : {self.account_balance}")
+    # def deposit(amount):
+    #     pass
+# BankAccount1 = BankAccount(200, "samir")
+# BankAccount1.printuser()
